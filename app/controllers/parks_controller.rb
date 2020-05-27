584b1368293bfe1408bd35265b73c0d2b980eff3
austinckohler <austinckohler@gmail.com>
2020-05-26 21:12:45 -0600

    parks are properly seeded and show on front end

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,2 +1,38 @@
 class ParksController < ApplicationController
+
+        before_action :find_park_by_id, only: [:show, :update, :destroy]
+    
+        def index
+            @parks = Park.all
+            render json: @parks
+        end
+        
+        def show
+            render json: @park
+        end
+        
+        def create
+            @park = Park.create(park_params)
+            render json: @park
+        end
+        
+        def update
+            @park.update(park_params)
+            render json: @park
+        end
+        
+        def destroy
+            @park.destroy
+            redirect_to action: "index"
+        end
+    
+        private 
+    
+        def park_params
+            params.require(:park).permit(:state, :url, :weather, :name, :latitude_longitude, :description)
+        end
+    
+        def find_park_by_id
+            Park.find(params[:id])
+        end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'rest-client'
+require 'json'
+
+def get_parks
+    response = RestClient.get "https://developer.nps.gov/api/v1/parks?limit=100&api_key=OJcmf4YsPUqMl90s4imakbUhVLGB0dewlTFqOPOa"
+    parsed_json = JSON.parse(response)
+
+    if !parsed_json.nil?
+        parsed_json["data"].map do |park|
+            Park.create(
+                state: park["states"],
+                url: park["url"],
+                weather: park["weatherInfo"],
+                name: park["fullName"],
+                lat_long: park["latLong"],
+                description: park["description"],
+                park_code: park["parkCode"]
+        )
+        end
+    else
+        puts "error seeding"
+    end
+end
+get_parks
